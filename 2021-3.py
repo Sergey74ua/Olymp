802f172e6b6e6a3e3142e3d85be2fbd876b05722
Sergey74ua <Sergey74ua@gmail.com>
2021-11-07 19:48:51 +0300

    Update 2021-3.py

--- 2021-3.py
+++ 2021-3.py
@@ -1,13 +1,28 @@
-def sPoint(N, M, arr):
-    for y, row in enumerate(arr):
-        minX = min(row)
-        for x in [ix for ix in range(M) if row[ix] == minX]:
-            if all(minX >= arr[iy][x] for iy in range(N)):
-                print(minX)
-                print(y, x)
-                return
-    print(0)
-
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for y in range(N)]
-sPoint(N, M, arr)
+test = False
+arrX = []
+minX = 0
+
+for y in range(N):
+    temp = min(arr[y])
+    if temp > minX:
+        minX = temp
+        arrX = [y]
+    elif temp == minX:
+        arrX.append(y)
+ 
+for x, i in enumerate(zip(*arr)):
+    tmp = max(i)
+    if tmp == minX:
+        for y in arrX:
+            if arr[y][x] == tmp:
+                print(arr[y][x])
+                print(y, x)
+                test = True
+                break
+    if test:
+        break
+
+if not test:
+    print(0)
