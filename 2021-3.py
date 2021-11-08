7df179f3fb685c1f821aa626159353286637eed0
Sergey74ua <Sergey74ua@gmail.com>
2021-11-08 18:39:03 +0300

    Update 2021-3.py

--- 2021-3.py
+++ 2021-3.py
@@ -1,28 +1,25 @@
 N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for y in range(N)]
-test = False
-arrX = []
-minX = 0
+arr = []
+for y in range(N):
+    arr.append(list(map(int, input().split())))
+
+maxY = posX = posY = posMinX = 0
+for y in range(N):
+    minX = 1000000000
+    for x in range(M):
+        if arr[y][x] < minX:
+            minX = arr[y][x]
+            posMinX = x
+    if minX > maxY:
+        maxY = minX
+        posX = posMinX
+        posY = y
 
 for y in range(N):
-    temp = min(arr[y])
-    if temp > minX:
-        minX = temp
-        arrX = [y]
-    elif temp == minX:
-        arrX.append(y)
- 
-for x, i in enumerate(zip(*arr)):
-    tmp = max(i)
-    if tmp == minX:
-        for y in arrX:
-            if arr[y][x] == tmp:
-                print(arr[y][x])
-                print(y, x)
-                test = True
-                break
-    if test:
+    if maxY < arr[y][posX]:
+        maxY = 0
         break
 
-if not test:
-    print(0)
+print(maxY)
+if maxY > 0:
+    print(posY, posX)
