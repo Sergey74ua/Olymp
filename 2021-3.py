15f7773e2dc31351fe09ec59b4edb2147ff449a3
Sergey74ua <Sergey74ua@gmail.com>
2021-11-11 20:26:26 +0300

    Update 2021-3.py

--- 2021-3.py
+++ 2021-3.py
@@ -1,25 +1,31 @@
-N, M = map(int, input().split())
 arr = []
+posX = [0]
+maxY = posY = test = 0
+
+N, M = map(int, input().split())
 for y in range(N):
     arr.append(list(map(int, input().split())))
 
-maxY = posX = posY = posMinX = 0
 for y in range(N):
     minX = 1000000000
     for x in range(M):
         if arr[y][x] < minX:
             minX = arr[y][x]
-            posMinX = x
+            tmpX = x
     if minX > maxY:
         maxY = minX
-        posX = posMinX
+        posX = [tmpX]
         posY = y
+    elif minX == maxY:
+        posX.append(tmpX)
 
-for y in range(N):
-    if maxY < arr[y][posX]:
-        maxY = 0
-        break
+for x in posX:
+    if all(maxY >= arr[y][x] for y in range(N)):
+        print(maxY)
+        print(posY, x)
+        test = 1
 
-print(maxY)
-if maxY > 0:
-    print(posY, posX)
+if test == 0:
+    print(0)
+
+#Нарушает безопасность
