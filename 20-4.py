188a693fff1b8a338985f49fb3a68eab95812008
Sergey74ua <Sergey74ua@gmail.com>
2022-01-12 23:59:06 +0300

    rename 20-4

--- 20-4.py
+++ 20-4.py
@@ -13,11 +13,11 @@ for i in range(len(arr)):
     tN = N - i * 2
     L = (tN - 1) * 4
     for x in range(tN):
-        matrix[i][(N - tN) // 2 + x] = (x - arr[i] + 1) % L
-        matrix[N - i][N - tN + x] = (L - x - arr[i]) % L ##################
-    #for y in range(tN - 2):
-    #    matrix[y][i] = (L - y * (tN - 2) - arr[i]) % L
-    #    matrix[y][tN - i] = (y * (tN - 2) - arr[i]) % L
+        matrix[i][(N - tN) // 2 + x] = (x - arr[i]) % L + 1
+        matrix[N - i - 1][(N - tN) // 2 + x] = (L - (tN - 2) - x - arr[i] - 1) % L + 1
+    for y in range(tN - 2):
+        matrix[y + i + 1][i] = (L - y - arr[i] - 1) % L + 1
+        matrix[y + i + 1][N - i - 1] = (tN + y - arr[i]) % L + 1
 
 for i in matrix:
     print(*i)
