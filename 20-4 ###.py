f4c55a42f6c3125f8a344f7568ab42fd25d6a968
Sergey74ua <Sergey74ua@gmail.com>
2022-01-12 20:03:03 +0300

    Update 20-4 ###.py

--- 20-4 ###.py
+++ 20-4 ###.py
@@ -5,15 +5,17 @@ for i in range(T):
     K, S = map(int, input().split())
     arr[K-1] = (arr[K-1] + S) % ((N - 1) * 4 - (K - 1) * 8)
 
-matrix = [[1] * N] * N
+matrix = [0] * N
+for i in range(N):
+    matrix[i] = [1] * N
 
 for i in range(len(arr)):
     tN = N - i * 2
     L = (tN - 1) * 4
-    for x in range(tN - 1):
-        matrix[i][(N - tN) // 2 + x] = 88 #(x - arr[i]) % L ПРОБЛЕМА С Х т.е. с    N - tN + x
-    #    matrix[N - i][N - tN + x] = (L - x - arr[i]) % L
-    #for y in range(tN - 1):
+    for x in range(tN):
+        matrix[i][(N - tN) // 2 + x] = (x - arr[i] + 1) % L
+        matrix[N - i][N - tN + x] = (L - x - arr[i]) % L ##################
+    #for y in range(tN - 2):
     #    matrix[y][i] = (L - y * (tN - 2) - arr[i]) % L
     #    matrix[y][tN - i] = (y * (tN - 2) - arr[i]) % L
 
