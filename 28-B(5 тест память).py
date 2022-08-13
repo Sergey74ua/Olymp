337a43c1101cb24c4c35c6cbbef92a9264668625
Sergey74ua <Sergey74ua@gmail.com>
2022-08-13 15:05:25 +0300

    Update 28-B(5 тест память).py

--- 28-B(5 тест память).py
+++ 28-B(5 тест память).py
@@ -2,12 +2,35 @@ T = int(input())
 for i in range(T):
     n, A = map(int, input().split())
     R = 0
+
+    TEST = 0
+    for j in str(int(''.join(['1']*n))*A):
+        TEST += int(j)
+    #print('ТУПАЯ ПРОВЕРКА - ', TEST)
+
     if len(str(A)) >= n:
         for j in str(int(''.join(['1']*n))*A):
             R += int(j)
-    else: ####################################
-        s = int(''.join(['1']*len(str(A))))*A
-        for j in str(s):
+        #print('простое умножение - ', int(''.join(['1']*n))*A)
+    else:
+        for j in str(int(''.join(['1']*len(str(A))))*A):
             R += int(j)
-        R += (n-len(str(A))*int(s[int(len(s)/2)]))
+        #print('урезанное умножение - ', int(''.join(['1']*len(str(A))))*A)
+        #print('сумма крайних - ', R)
+        s = int(''.join(['1']*(len(str(A))+1)))*A
+        #print('избыточное умножение с центральной(повторяющейся) цифрой - ', s)
+
+        pos = len(str(s))//2
+        #print('позиция повторяшки - ', pos)
+
+        num = int(str(s)[pos])
+        #print('цифра - повторяшка - ', num)
+
+        count = int(n) - len(str(A))
+        #print('число повторяшек - ', count)
+
+        summ = count * num
+        #print('сумма повторяшек - ', summ)
+
+        R += summ
     print(R)
