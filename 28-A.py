6210d0d0181f60532cf364ca4159df81ec7d6364
Sergey74ua <Sergey74ua@gmail.com>
2022-08-25 15:42:14 +0300

    Update 28-A.py

--- 28-A.py
+++ 28-A.py
@@ -1,16 +1,7 @@
 A, B=map(int, input().split())
 s=min(A%B, B-A%B)
-m=s
 for i in range(s):
     Bp=B+i
     Bm=B-i
-    ApBp=A%Bp+i
-    AmBp=Bp-A%Bp+i
-    ApBm=A%Bm+i
-    AmBm=Bm-A%Bm+i
-    t=min(ApBp, AmBp, ApBm, AmBm)
-    if t<m:
-        m=t
-        if m==0:
-            break
-print(m)
+    s=min(s, A%Bp+i, Bp-A%Bp+i, A%Bm+i, Bm-A%Bm+i)
+print(s)
