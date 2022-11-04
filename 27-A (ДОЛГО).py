12b43aa3b229984657ade85e5aaff9e6876725ca
Sergey74ua <Sergey74ua@gmail.com>
2022-11-04 19:27:57 +0300

    sort olimp

--- 27-A (ДОЛГО).py
+++ 27-A (ДОЛГО).py
@@ -2,21 +2,22 @@ T = int(input())
 for i in range(T):
   N = int(input())
   arr = []
-  r = 11
+  r = 22222
   for j in range(N):
     s = input()
     if len(s) < r:
       r = len(s)
     for k in range(len(arr)):
       if s[:r] == arr[k][:r]:
-        r = 'NO'
+        r = -1
         break
-    if r == 'NO':
+    if r == -1:
       print('NO')
       break
     else:
-      arr.append(s)
-  if r != 'NO':
+      arr.append(s[:r])
+  if r > 0:
     print('YES')
 
 # Не проходит по времени на 2 тесте
+# ... нужно дерево
---
+++ 27-D.py
@@ -0,0 +1,5 @@
+S = input()
+T = input()
+
+while len(arr) < len(T):
+
