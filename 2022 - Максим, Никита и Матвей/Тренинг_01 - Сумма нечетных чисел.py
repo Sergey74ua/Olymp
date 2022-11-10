66a04acf3a5e3115c0b3b32728a0ea526e3560ee
Sergey74ua <Sergey74ua@gmail.com>
2022-11-10 19:33:58 +0300

    Create Тренинг_01 - Сумма нечетных чисел.py

---
+++ 2022 - Максим, Никита и Матвей/Тренинг_01 - Сумма нечетных чисел.py
@@ -0,0 +1,22 @@
+T=int(input())
+for i in range(T):
+    L, R = map(int, input().split())
+    if L%2==0:
+        L+=1
+    if R%2==0:
+        R-=1
+    print(((R-L)//2+1)*(L+R)//2)
+
+
+#Неполное решение - 14  Превышено максимальное время работы >1.000
+#T=int(input())
+#for i in range(T):
+#    L, R = map(int, input().split())
+#   if L%2==0:
+#        L+=1
+#   if R%2==0:
+#        R-=1
+#   s=0
+#   for j in range(L, R+1, 2):
+#       s+=j
+#   print(s)
