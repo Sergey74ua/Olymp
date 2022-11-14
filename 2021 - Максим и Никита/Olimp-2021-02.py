0bb8aab04041d504d6ffe8030fa76878eeae7fec
Sergey74ua <Sergey74ua@gmail.com>
2022-11-14 20:16:03 +0300

    Update Olimp-2021-02.py

--- 2021 - Максим и Никита/Olimp-2021-02.py
+++ 2021 - Максим и Никита/Olimp-2021-02.py
@@ -1,28 +1,13 @@
-arr = []
+arr=[]
 for y in range(3):
-    arr.append(list(map(int, input().split())))
-
-S = []
-for x in range(3):
-    temp = 0
-    for y in range(3):
-        temp += arr[y][x]
-    S.append(temp)
-xMin = S.index(min(S))
-xMax = S.index(max(S))
-
-for yMin in range(3):
-    test = 0
-    for yMax in range(3):
-        arr[yMin][xMin], arr[yMax][xMax] = arr[yMax][xMax], arr[yMin][xMin]
-        if arr[0][xMin]+arr[1][xMin]+arr[2][xMin] == arr[0][xMax]+arr[1][xMax]+arr[2][xMax]:
-        # Возможно еще надо проверять сумму строк, а не только колонок
-            test = 1
+    for x in input().split():
+        arr.append(int(x))
+for i in range(9):
+    for j in range(i+1, 9):
+        arr[i], arr[j]=arr[j], arr[i]
+        if sum(arr[0:3])==sum(arr[3:6])==sum(arr[6:9]) and arr[0]+arr[3]+arr[6]==arr[1]+arr[4]+arr[7]==arr[2]+arr[5]+arr[8]:
             break
         else:
-            arr[yMax][xMax], arr[yMin][xMin] = arr[yMin][xMin], arr[yMax][xMax]
-    if test > 0:
-        break
-
-for y in range(3):
-    print(*arr[y])
+            arr[i], arr[j]=arr[j], arr[i]
+for i in range(0, 9, 3):
+    print(str(arr[i])+' '+str(arr[i+1])+' '+str(arr[i+2]))
