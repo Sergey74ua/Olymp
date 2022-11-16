295d7d32ed5de3c5716a741fa620992e535d6965
Sergey74ua <Sergey74ua@gmail.com>
2022-11-16 17:41:25 +0300

    Update Olimp-2021-05.py

--- 2021 - Максим и Никита/Olimp-2021-05.py
+++ 2021 - Максим и Никита/Olimp-2021-05.py
@@ -1,15 +1,34 @@
-N = int(input())
-arrN = list(map(int, input().split()))
-M = int(input())
-arrM = list(map(int, input().split()))
-K = int(input())
+N=int(input())
+arrN=list(map(int, input().split()))
+arrN.sort()
+M=int(input())
+arrM=list(map(int, input().split()))
+arrM.sort()
+K=int(input())
+n=m=0
+print()
+print(arrN)
+print(arrM)
+print()
+while n+m<K:
+    s=arrN[n]+arrM[m]
+    print(n, m, ' - ', arrN[n], arrM[m], ' - ', s) # ПРОПУСКИ, не каждое с каждым суммируется.
+    if arrN[n+1]+arrM[m]<=arrN[n]+arrM[m+1]:
+        n+=1
+    else:
+        m+=1
+print(s)
 
-arr = []
-for i in arrN:
-    for j in arrM:
-        arr.append(i + j)
-        #вставка в массив с бинарным поиском позиции
-       
-arr.sort()
 
-print(arr[K-1])
+# Неполное решение - 36 Превышено максимальное время работы >3.000  0 (1)
+#N = int(input())
+#arrN = list(map(int, input().split()))
+#M = int(input())
+#arrM = list(map(int, input().split()))
+#K = int(input())
+#arr = []
+#for i in arrN:
+#    for j in arrM:
+#        arr.append(i + j) # Может вставка в массив с бинарным поиском позиции
+#arr.sort()
+#print(arr[K-1])
