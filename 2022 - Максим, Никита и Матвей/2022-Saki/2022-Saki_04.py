48aa47692df5911834b2428d1c906cbebd44d91c
Sergey74ua <Sergey74ua@gmail.com>
2022-12-22 16:59:34 +0300

    4 - 56/60

--- 2022 - Максим, Никита и Матвей/2022-Saki/2022-Saki_04.py
+++ 2022 - Максим, Никита и Матвей/2022-Saki/2022-Saki_04.py
@@ -1,14 +1,36 @@
+import math
 N=int(input())
 r=1
 for i in range(2, int((N)**0.5)+1):
-  t=2
-  s=0
-  while s<=N:
-    s=i**t
-    if s==N:
-      r=max(r, t)
-      t=0
-      break
-    else:
-      t+=1
+  s=round(math.log(N, i))
+  if i**s==N:
+    r=s
+    break
 print(r)
+
+#57 Превышено максимальное время работы >2.000  0 (1)
+#import math
+#N=int(input())
+#r=1
+#for i in range(2, int((N)**0.5)+1):
+#  s=round(math.log(N, i))
+#  if i**s==N:
+#    r=s
+#    break
+#print(r)
+
+#48 Превышено максимальное время работы >2.000  0 (1)
+#N=int(input())
+#r=1
+#for i in range(2, int((N)**0.5)+1):
+#  t=2
+#  s=0
+#  while s<=N:
+#    s=i**t
+#    if s==N:
+#      r=max(r, t)
+#      t=0
+#      break
+#    else:
+#      t+=1
+#print(r)
